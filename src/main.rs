9b95fb00d94519faea9411220d98905fefec8bd7
Voyage <93342430+AlexanderMaxRanabel@users.noreply.github.com>
2023-06-13 11:36:24 +0300

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use std::process::Command;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::io;
+use std::process;
+
+mod install;
+
+fn is_nixos() -> bool {
+    if let Ok(file) = File::open("/etc/os-release") {
+        let reader = BufReader::new(file);
+        for line in reader.lines() {
+            if let Ok(line) = line {
+                if line.starts_with("ID=nixos") {
+                    return true;
+                }
+            }
+        }
+    }
+    false
+}
+
+fn main() {
+    if is_nixos() {
+        let mut installer_choice = String::new();
+        println!("Welcome to VynxOS Installer!");
+        println!("Please be sure you runned VynxOS Installer with sudo otherwise it wont simply work");
+        println!("Migrate from 22.11 to 23.05 or New Installition?");
+        io::stdin().read_line(&mut installer_choice).expect("Error While reading input");
+        let installer_choice = installer_choice.trim();
+
+        match installer_choice {
+            "install" => { 
+                install::install();
+            },
+
+            "migrate" => {
+               
+                let channel_update = Command::new("nix-channel")
+                    .arg("--add")
+                    .arg("https://nixos.org/channels/nixos-23.05")
+                    .arg("nixos")
+                    .output()
+                    .expect("Failed");
+                if channel_update.status.success() {
+                    let small_update = Command::new("nix-channel")
+                        .arg("--update")
+                        .output()
+                        .expect("Failed");
+                    if small_update.status.success() {
+                        let build = Command::new("nixos-rebuild")
+                            .arg("switch")
+                            .output()
+                            .expect("Failed");
+
+                        if build.status.success() {
+                            let reboot = Command::new("reboot")
+                                .output()
+                                .expect("Failed");
+                            if reboot.status.success() {
+
+                            } else {
+                                let stderr = String::from_utf8_lossy(&channel_update.stderr);
+                                eprintln!("Failed to run command. Error: {}", stderr);
+                                process::exit(1);
+                            }
+                        } else {
+                            let stderr = String::from_utf8_lossy(&channel_update.stderr);
+                            eprintln!("Failed to run command. Error: {}", stderr);
+                            process::exit(1);
+                        } 
+                    } else {
+                       let stderr = String::from_utf8_lossy(&channel_update.stderr);
+                        eprintln!("Failed to run command. Error: {}", stderr);
+                        process::exit(1); 
+                    }
+                } else {
+                    let stderr = String::from_utf8_lossy(&channel_update.stderr);
+                    eprintln!("Failed to run command. Error: {}", stderr);
+                    process::exit(1);
+                }
+
+            },
+            _ => {
+                println!("Not a valid choice");
+                process::exit(1);
+            }
+        }
+    } else {
+        println!("Error: Distro is not NixOS");
+        process::exit(1);
+    }
+    
+}
